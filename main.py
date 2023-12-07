b71721b2da2103f62ede0e40446865a5612144c2
Jules LE BRIS <jls.lebris@gmail.com>
2023-12-07 13:06:59 +0100

    print reduce taux

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 discount = {1000: 0.03, 5000:0.05, 7000:0.07, 10000:0.1, 50000:0.15}
 tax = {"UT": 0.0685, "NV": 0.008, "TX": 0.0625, "AL": 0.04, "CA": 0.0825}
 
+def get_discount(value):
+    if value >= 50000:
+        return 0.15
+    if value >= 10000:
+        return 0.1
+    if value >= 7000:
+        return 0.07
+    if value >= 5000:
+        return 0.05
+    if value >= 1000:
+        return 0.03
+    return 0
+
 
 def main():
     nb_art = int(input("nombre d'article : "))
@@ -15,4 +28,7 @@ def main():
     prix_ttc = total + calc_tax
     print(f"prix TTC : {prix_ttc}€")
 
+    reduce_taux = get_discount(prix_ttc)
+    print(f"your reduce taux is {reduce_taux}")
+    
 main()
