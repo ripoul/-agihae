b228eab4164206bad07ce7537edcfc1d109c5aaf
Jules LE BRIS <jls.lebris@gmail.com>
2023-12-07 12:51:03 +0100

    add imput nb article

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+discount = {1000: 0.03, 5000:0.05, 7000:0.07, 10000:0.1, 50000:0.15}
+tax = {"UT": 0.0685, "NV": 0.008, "TX": 0.0625, "AL": 0.04, "CA": 0.0825}
+
+
 def main():
-    print("hello word")
+    print(int(input("nombre d'article : ")))
 
 main()
